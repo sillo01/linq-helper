18bb1b768c9d3df7ec626074c34989f59f49caae
Alansillo <ramos.acrp@gmail.com>
2017-07-28 10:27:57 -0500

    cache manager

---
+++ SqlRuntimeCacheManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Data.Linq;
+using System.Runtime.Caching;
+using System.Data.SqlClient;
+
+namespace LinqHelper
+
+{
+    public sealed class SqlRuntimeCacheManager
+    {
+        private static string dbName = "CachedDatabase";
+        private static string ConnectionString;
+        private static DataContext _DataContext;
+        private static SqlRuntimeCacheManager _Instance;
+        private static HashSet<string> _RegisteredTables;
+
+        public static DataBaseEntity dbEntity
+        {
+            get
+            {
+                var ctx = DataContextFactory.GetScopedDataContext<DataBaseEntity>(dbName);
+                if (ctx.Context == null)
+                {
+                    ctx.Context = _DataContext;
+                }
+
+                return ctx;
+            }
+        }
+        public static SqlRuntimeCacheManager Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+        private HashSet<string> RegisteredTables { get; }
+
+        // Initialization
+        public static void GenerateInstance(DataContext Context)
+        {
+            _Instance = new SqlRuntimeCacheManager(Context);
+            _RegisteredTables = new HashSet<string>();
+        }
+        private SqlRuntimeCacheManager(DataContext Context)
+        {
+            SqlRuntimeCacheManager.ConnectionString = Context.Connection.ConnectionString;
+            _DataContext = Context;
+        }
+
+        // SQL Dependencies
+        public void RegisterDependency()
+        {
+            SqlDependency.Start(ConnectionString);
+        }
+        public void DisableCacheNotifications()
+        {
+            SqlDependency.Stop(ConnectionString);
+        }
+
+        public List<T> GetAllCached<T>()
+            where T : class, IDataEntity
+        {
+            var tableName = dbEntity.GetTableName<T>();
+
+            List<T> result = null;
+            result = (List<T>)MemoryCache.Default.Get(tableName);
+
+
+            if (result == null)
+            {
+                using (var conn = new SqlConnection(ConnectionString))
+                {
+                    using (var cmd = new SqlCommand())
+                    {
+                        conn.Open();
+                        cmd.Connection = conn;
+                        cmd.CommandText = dbEntity.Context.GetCommand(dbEntity.Context.GetTable<T>()).CommandText;
+                        cmd.Notification = null;
+
+                        SqlDependency dep = new SqlDependency();
+                        dep.AddCommandDependency(cmd);
+
+                        //MemoryCache.Default.Set(table, value, policy);
+                        var policy = new CacheItemPolicy()
+                        {
+                            AbsoluteExpiration = ObjectCache.InfiniteAbsoluteExpiration,
+                            Priority = System.Runtime.Caching.CacheItemPriority.Default,
+                            SlidingExpiration = new TimeSpan(0, 20, 0),
+                            RemovedCallback = (CacheEntryRemovedArguments args) =>
+                            {
+                                MemoryCache.Default.Remove(tableName);
+                            }
+                        };
+                        SqlChangeMonitor mon = new SqlChangeMonitor(dep);
+                        policy.ChangeMonitors.Add(mon);
+
+                        result = dbEntity.Context.Translate<T>(cmd.ExecuteReader()).ToList();
+                        MemoryCache.Default.Set(tableName, result, policy);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SqlWebCacheManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Linq.Dynamic;
+using System.Data.Linq;
+
+
+namespace LinqHelper
+
+{
+    public class SqlWebCacheManager
+    {
+        private static string dbName = "CachedDatabase";
+        private static string ConnectionString;
+        private static DataContext _DataContext;
+        private static SqlWebCacheManager _Instance;
+
+        public static DataBaseEntity dbEntity
+        {
+            get
+            {
+                var ctx = DataContextFactory.GetScopedDataContext<DataBaseEntity>(dbName);
+                if (ctx.Context == null)
+                {
+                    ctx.Context = _DataContext;
+                }
+                
+                return ctx;
+            }
+        }
+
+        public static SqlWebCacheManager Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+        public static void GenerateInstance(DataContext Context)
+        {
+            _Instance = new SqlWebCacheManager(Context);
+        }
+        private SqlWebCacheManager(DataContext Context)
+        {
+            SqlWebCacheManager.ConnectionString = Context.Connection.ConnectionString;
+            _DataContext = Context;
+        }
+
+        public void RegisterDependency()
+        {
+            SqlCacheDependencyAdmin.EnableNotifications(ConnectionString);
+        }
+        public void DisableCacheNotifications()
+        {
+            SqlCacheDependencyAdmin.DisableNotifications(ConnectionString);
+        }
+        public void RegisterTableDependency(string table)
+        {
+            var currentTables = SqlCacheDependencyAdmin
+                .GetTablesEnabledForNotifications(ConnectionString);
+            if (!currentTables.Contains(table))
+            {
+                SqlCacheDependencyAdmin
+                    .EnableTableForNotifications(ConnectionString, table);
+            }
+        }
+        public void SaveCache(string name, object value, CacheItemRemovedCallback callback)
+        {
+            RegisterTableDependency(name);
+            var dependency = new SqlCacheDependency(dbName, name);
+            HttpRuntime.Cache.Add(
+                name, value, dependency, Cache.NoAbsoluteExpiration, new TimeSpan(0, 20, 0), CacheItemPriority.Normal, callback
+                );
+        }
+        void OnCacheRemoved(string name, object cachedObject, CacheItemRemovedReason reason)
+        {
+            HttpRuntime.Cache.Remove(name);
+        }
+
+        public List<T> GetAllCached<T>()
+            where T : class, IDataEntity
+        {
+            var tableName = dbEntity.GetTableName<T>();
+            List<T> result = null;
+            result = (List<T>)HttpRuntime.Cache[tableName];
+
+            if (result == null)
+            {
+                var elements = dbEntity.GetAll<T>().ToList().OrderBy(e => e.Id);
+                result = (List<T>)Activator.CreateInstance(typeof(List<>).MakeGenericType(typeof(T)));
+                result.AddRange(elements);
+                SaveCache(tableName, result, OnCacheRemoved);
+            }
+
+            return result;
+        }
+    }
+}
