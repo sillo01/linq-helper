f53c19e62ce8448b7fd826d65b58ad2a1c2ac028
Alansillo <ramos.acrp@gmail.com>
2017-08-01 19:03:31 -0500

    new TableDefinition

--- Context/TableDefinition.cs
+++ Context/TableDefinition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace LinqHelper.Context
 {
     public class TableDefinition
     {
+        private readonly Type _type;
+        public string TableName;
+        public string CommandText;
+
+        public TableDefinition(System.Type type, DataContext context)
+        {
+            _type = type;
+            var table = context.Mapping.GetTable(type);
+            this.TableName = table.TableName;
+            CommandText = context.GetCommand(context.GetTable(type)).CommandText;
+        }
     }
+
 }
