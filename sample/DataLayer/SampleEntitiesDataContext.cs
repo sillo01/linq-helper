f14d190353a10fa8059d9c3ba3aff3cb177115fc
Alansillo <ramos.acrp@gmail.com>
2017-08-07 17:50:05 -0500

    data context

---
+++ sample/DataLayer/SampleEntitiesDataContext.cs
@@ -0,0 +1,34 @@
+﻿using Guayaba.LinqHelper;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public partial class SampleEntitiesDataContext
+    {
+        public static SampleEntitiesDataContext Current
+        {
+            get
+            {
+                SampleEntitiesDataContext ctx = (SampleEntitiesDataContext)MemoryCache.Default["DataContextDataContext"];
+                if (ctx == null)
+                {
+                    ctx = new SampleEntitiesDataContext();
+                    CacheItemPolicy policy = new CacheItemPolicy();
+                    MemoryCache.Default.Set("DataContextDataContext", ctx, policy);
+                }
+                return ctx;
+            }
+        }
+        public static void RegisterDependency()
+        {
+            LinqHelper.GenerateInstance(Current);
+            LinqHelper.Instance.RegisterDependency();
+        }
+    }
+}
---
+++ sample/DataLayer/partials/CountryCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guayaba.LinqHelper;
+
+namespace DataLayer
+{
+    public partial class CountryCode : IDataEntity
+    {
+        public string Id { get { return IdCountryCode.ToString(); } }
+
+        public List<CountryCode> Cached
+        {
+            get
+            {
+                return LinqHelper.Instance.GetAllCached<CountryCode>();
+            }
+        }
+    }
+}
